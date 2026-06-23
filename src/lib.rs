6e85ad57f4465112e0a195335a5566ee32b8ef5f
ajay-nagpal <ajaynagpal.an64@gmail.com>
2026-06-23 19:45:47 +0530

    parse the cli args

---
+++ src/lib.rs
@@ -0,0 +1,71 @@
+use getopts::Options;              //Cli arguments parsing
+
+//to print help message when cli is invalid
+pub fn  print_err_msg(){
+  println!("invalid cli!");
+  println!("required flags : -s <s> -E <E> -b <b> -t <tracefile>");
+}
+
+//This function parse the command line args and extracts the cache parameters
+// this design is expandable for allowing future extension in cli, like adding -p flag for policy
+// we return None form this function in any invalid input case
+pub fn parse_args(args:&Vec<String>)->Option<(usize,usize,usize,String)>{
+  
+  //strict enforce to take all 4 required flags 
+  if args.len()!=9{
+    print_err_msg();
+    return None;
+  }
+
+  let mut opts=Options::new();
+
+  //defining expected options
+  opts.optopt("s","","","");
+  opts.optopt("b","","","");
+  opts.optopt("E","","","");
+  opts.optopt("t","","","");
+
+  // parse the arguments safely
+  let matches:getopts::Matches=match opts.parse(&args[1..]){
+    Ok(m)=>m,
+    Err(_)=>{
+      print_err_msg();
+      return None;
+    }
+  };
+
+  //reject unexpected arguments and return None if any invalid arg in cli.
+  if !matches.free.is_empty(){
+    print_err_msg();
+    return None;
+  }
+
+  //retrive valusfor each required flag
+  let s=matches.opt_str("s");
+  let b=matches.opt_str("b");
+  let e=matches.opt_str("E");
+  let t=matches.opt_str("t");
+
+  //ensure all required flags are present
+  if s.is_none()||b.is_none()||e.is_none()||t.is_none(){
+    print_err_msg();
+    return None;
+  }
+
+  //parse numeric value safely, handle error by returning None
+  let (s,e,b,trace_file):(usize,usize,usize,String)=match(
+    s.unwrap().parse(),
+    e.unwrap().parse(),
+    b.unwrap().parse(),
+    t.unwrap(),
+  ){
+    (Ok(sv),Ok(ev),Ok(bv),tv)=>(sv,ev,bv,tv),
+    _ =>{
+      print_err_msg();
+      return None;
+    }
+  };
+
+  //return Some(value) of validated cli flag values. 
+  Some((s,e,b,trace_file))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 use std::env;                   //read command line arguments
+use multi_level_cache_simulator::{print_err_msg, parse_args};// import utility fucntion used here 
 pub fn main() {
 
   //collect command line args into a vector
   let args:Vec<String>=env::args().collect();
+  // use parse_args utility function to extrace the values of 
+  //s: number of set index bits
+  //e: number of lines in a set
+  //b: number of block offset bits
+  // trace_file: path to the file containing memory access
+  let (s,e,b,trace_file):(usize,usize,usize,String)=match parse_args(&args){
+    //successful parse, extract values
+    Some(v)=>v,
+
+    // anything invalid, return to prevent undefined behaviour
+    None=>{
+      print_err_msg();
+      return;
+    }
+  };
+
+  if s==0 || b==0||e==0|| b>=64||(s+b)>=64 {
+    println!("invalid flag(s|b|E) value!");
+    return;
+  }
 }
